10637176d904ca57a4c4e647ea9ce45a9d0a48fd
Ionut Traistaru <ionut.traistaru@turntown.digital>
2025-10-27 09:23:22 +0000

    Sync dotfiles from main

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -153,7 +153,18 @@ return {
     --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
     local servers = {
       -- clangd = {},
-      -- gopls = {},
+      gopls = {
+        settings = {
+          gopls = {
+            analyses = {
+              unusedparams = true,
+              shadow = true,
+            },
+            staticcheck = true,
+            gofumpt = true,
+          },
+        },
+      },
       -- pyright = {},
       -- rust_analyzer = {},
       -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
@@ -186,6 +197,14 @@ return {
       sqlls = {},
       jsonls = {},
       yamlls = {},
+
+      -- Zig Language Server
+      zls = {},
+
+      -- Angular Language Server
+      angularls = {
+        filetypes = { 'typescript', 'html', 'typescriptreact', 'typescript.tsx' },
+      },
 
       lua_ls = {
         -- cmd = {...},
