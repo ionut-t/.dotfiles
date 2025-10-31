89d19e9b4c71b8740006d9303176501eedd6393b
Ionut Traistaru <ionut.traistaru@turntown.digital>
2025-10-31 14:26:40 +0000

    Temporary remove nvim config

--- nvim/.config/nvim/lua/plugins/zig.lua
+++
@@ -1,9 +0,0 @@
-return {
-  'ziglang/zig.vim',
-  ft = 'zig',
-  config = function()
-    -- Zig.vim provides syntax highlighting and basic integration
-    -- ZLS (Zig Language Server) should be installed via Mason and configured in lsp.lua
-    vim.g.zig_fmt_autosave = 1
-  end,
-}
