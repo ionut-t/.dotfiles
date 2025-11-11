c82b40c1eda6194cc0f5168c9b7392f446a1bb98
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-11-11 08:20:37 +0000

    refactor(plugins): manage treesitter context toggle with snacks
    The treesitter-context toggle keymap `<leader>uc` is now managed
    by snacks.nvim. This centralises plugin toggle management,
    providing a more consistent user experience across various features.
    
    - Remove toggle keymap from treesitter-context configuration
    - Introduce a new snacks.toggle for treesitter context
    - Adjust 'go to context' keymap to use vim.keymap.set directly

--- nvim/.config/nvim/lua/plugins/treesitter-context.lua
+++ nvim/.config/nvim/lua/plugins/treesitter-context.lua
@@ -13,21 +13,11 @@ return {
       separator = nil, -- Use a string like '-' to add a separator line
       zindex = 20,
     })
+
+    -- Set up keymap for jumping to context
+    -- Note: Toggle is handled by snacks.nvim (<leader>uc)
+    vim.keymap.set('n', '[C', function()
+      require('treesitter-context').go_to_context()
+    end, { desc = 'Go to context' })
   end,
-  keys = {
-    {
-      '<leader>uc',
-      function()
-        require('treesitter-context').toggle()
-      end,
-      desc = 'Toggle context',
-    },
-    {
-      '[C',
-      function()
-        require('treesitter-context').go_to_context()
-      end,
-      desc = 'Go to context',
-    },
-  },
 }
