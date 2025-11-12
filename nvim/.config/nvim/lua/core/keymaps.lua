db8956699748a1b0d2520a5885f94e4ad3922432
Ionut Traistaru <ionut.traistaru@turntown.digital>
2025-11-12 09:27:21 +0000

    Sync dotfiles from main

--- nvim/.config/nvim/lua/core/keymaps.lua
+++ nvim/.config/nvim/lua/core/keymaps.lua
@@ -252,7 +252,7 @@ vim.keymap.set('n', '<leader>bo', function()
   local current = vim.api.nvim_get_current_buf()
   local buffers = vim.api.nvim_list_bufs()
   for _, buf in ipairs(buffers) do
-    if buf ~= current and vim.api.nvim_buf_is_loaded(buf) and vim.api.nvim_buf_get_option(buf, 'buftype') == '' then
+    if buf ~= current and vim.api.nvim_buf_is_loaded(buf) and vim.bo[buf].buftype == '' then
       vim.cmd('Bdelete ' .. buf)
     end
   end
