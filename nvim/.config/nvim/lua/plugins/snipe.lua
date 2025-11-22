bd32a78983babbedbfecbedc0cb76550ee6bf77e
Ionut Traistaru <ionut.m.traistaru@gmail.com>
2025-11-22 09:26:08 +0000

    fix(nvim): adjust snipe ui window position
    Change the snipe UI window position from 'topleft' to 'topright'. This adjustment aims to provide a more conventional and less intrusive user experience within the Neovim environment.

--- nvim/.config/nvim/lua/plugins/snipe.lua
+++ nvim/.config/nvim/lua/plugins/snipe.lua
@@ -18,7 +18,7 @@ return {
         -- Where to place the ui window
         -- Can be any of "topleft", "bottomleft", "topright", "bottomright", "center", "cursor" (sets under the current cursor pos)
         ---@type "topleft"|"bottomleft"|"topright"|"bottomright"|"center"|"cursor"
-        position = 'topleft',
+        position = 'topright',
         -- Override options passed to `nvim_open_win`
         -- Be careful with this as snipe will not validate
         -- anything you override here. See `:h nvim_open_win`
